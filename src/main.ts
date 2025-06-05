09463209059c6e35ab0834df9fe0661b68f1ef4c
qranyue <qran.yue@hotmail.com>
2025-06-05 15:56:48 +0800

    feat: 初始化Vue项目并添加路由

---
+++ src/main.ts
@@ -0,0 +1,11 @@
+import { createApp } from "vue";
+
+import "@arco-design/web-vue/es/index.css";
+import App from "./App.vue";
+import router from "./router";
+
+const app = createApp(App);
+
+app.use(router);
+
+app.mount("#app");
